1e228f92e217925f25c9c0987478fc36eba575c6
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-01-24 21:05:21 +0900

    22/01/24

---
+++ 백준/81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return True
+        
+        return False
