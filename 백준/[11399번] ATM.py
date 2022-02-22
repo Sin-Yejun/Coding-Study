56aa92f8c48482eab237ef3d1ea564863d51f130
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-02-22 23:07:28 +0900

    22/02/22

--- 백준/[11399번] ATM.py
+++ 백준/[11399번] ATM.py
@@ -1,7 +1,13 @@
 n = int(input())
 arr = list(map(int,input().split()))
+
 arr.sort()
 s = 0
-for i in range(1,n):
-    s+=sum(arr[:i])
-print(s+sum(arr))
+for i in range(len(arr)):
+    s += arr[i]
+    for j in range(i):
+        s += arr[j]
+
+print(s)
+    
+        
