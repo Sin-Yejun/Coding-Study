cf2944e0db574ae172a954b0bf02c4eea535ae87
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-03-01 21:20:20 +0900

    22/03/01

---
+++ 백준/[18258번] 큐 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+
+queue = deque()
+
+for _ in range(n):
+    command = list(sys.stdin.readline().split())
+    if len(command) == 2:
+        command[1] = int(command[1])
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+            
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
