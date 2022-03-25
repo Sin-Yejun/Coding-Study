a5a9b2ab6d4a885aef9ca99b8748042b69775e39
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-03-25 14:36:00 +0900

    22/03/25

---
+++ Leetcode/257. Binary Tree Paths.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/binary-tree-paths/discuss/68272/Python-solutions-(dfs%2Bstack-bfs%2Bqueue-dfs-recursively).
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+        res, queue = [], deque([(root, "")])
+        
+        while queue:
+            node, ls = queue.popleft()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.left:
+                queue.append((node.left, ls+str(node.val)+"->"))
+            if node.right:
+                queue.append((node.right, ls+str(node.val)+"->"))
+        return res
+        
