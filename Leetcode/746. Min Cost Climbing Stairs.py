dbbfbeb8dafabdb063f3e1bee8bcbddb65e5b2cb
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-03-28 21:24:25 +0900

    22/03/28

---
+++ Leetcode/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs/discuss/657490/Python-solution-from-a-beginner-(some-easy-steps-to-follow-to-solve-dp)
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        dp = [0] * len(cost)
+        
+        dp[0] = cost[0]
+        
+        if len(cost) >= 2:
+            dp[1] = cost[1]
+        
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        
+        return min(dp[-1], dp[-2])
