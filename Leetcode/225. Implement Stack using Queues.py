f2da100b9f2ca60862f984dcd563f5e2c62dfc03
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-04-07 16:13:47 +0900

    22/04/07

---
+++ Leetcode/225. Implement Stack using Queues.py
@@ -0,0 +1,28 @@
+class MyStack:
+
+    def __init__(self):
+        self.arr = []
+
+    def push(self, x: int) -> None:
+        self.arr.append(x)
+
+    def pop(self) -> int:
+        x = self.arr.pop()
+        return x
+
+    def top(self) -> int:
+        return self.arr[-1]
+
+    def empty(self) -> bool:
+        if self.arr:
+            return False
+        else:
+            return True
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
