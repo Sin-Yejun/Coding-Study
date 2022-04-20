3bbd5782c781dbffc2a53672a232ae65c7ea53dc
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-04-20 21:50:31 +0900

    22/04/20

---
+++ 백준/[6064번] 카잉 달력.py
@@ -0,0 +1,12 @@
+# https://pacific-ocean.tistory.com/126
+def num(m, n, x, y):
+    while x <= m * n:
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
+
+t = int(input())
+for i in range(t):
+    m, n, x, y = map(int, input().split())
+    print(num(m, n, x, y))
