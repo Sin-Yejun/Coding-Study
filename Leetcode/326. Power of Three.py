f62f7987c1045ed3729b5e513a6efd3b92f43f07
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-07-29 16:49:34 +0900

    22/07/29

---
+++ Leetcode/326. Power of Three.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n == 0:
+            return False
+        else:
+            return n%3 == 0 and self.isPowerOfThree(n//3)
