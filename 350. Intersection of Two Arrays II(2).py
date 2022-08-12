8896b217db6e0549e3a501ec3be0c09a0f61265d
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-08-12 16:53:59 +0900

    Delete 350. Intersection of Two Arrays II(2).py

--- 350. Intersection of Two Arrays II(2).py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        answer = []
-        for i in nums1:
-            if i in nums2:
-                answer.append(i)
-                nums2.remove(i)
-        return answer
