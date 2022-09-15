1815dcb32eec57132ae4bb0729920f698cbe9a6b
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-09-15 15:27:21 +0900

    22/09/15

---
+++ Leetcode/143. Reorder List.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/reorder-list/discuss/801883/Python-3-steps-to-success-explained
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        #find middle
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse second-half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        slow.next = None
+        
+        # merge
+        head1, head2 = head, prev
+        while head2:
+            nextt = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = nextt
