7f9394ecf5261bbf1729e6c0096f7c6fe1ce2c0d
yejun <54695795+Sin-Yejun@users.noreply.github.com>
2022-09-23 21:37:58 +0900

    22/09/23

---
+++ Leetcode/1680. Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        binary = ''
+        for i in range(1, n+1):
+            binary += bin(i)[2:]
+        
+        return int(binary,2) % (10**9+7)
